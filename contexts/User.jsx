448722b068e9b44a460e9881918980fcc99fad3e
bluluke <bluluke@hotmail.com>
2023-08-03 14:57:40 +0100

    user-context available as the user_name murling0

---
+++ contexts/User.jsx
@@ -0,0 +1,12 @@
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+export const UserContextProvider = ({ children }) => {
+  const [user, setUser] = useState("murling0");
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
