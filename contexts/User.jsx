57189e718ecb8b94c5cd8560076abd5930d06d7f
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-08-04 16:40:51 +0100

    add functionality for fetching user position with coordinate or postcode

--- contexts/User.jsx
+++ contexts/User.jsx
@@ -1,11 +1,23 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useState } from "react";
 
 export const UserContext = createContext();
 
 export const UserContextProvider = ({ children }) => {
-  const [user, setUser] = useState("murling0");
+  const [user, setUser] = useState({
+    _id: "64c7abf68c2d17441844e6fd",
+    first_name: "Marchelle",
+    last_name: "Urling",
+    email: "murling0@businessinsider.com",
+    user_name: "murling0",
+    address: "Riga Street, Manchester, United Kingdom",
+    postcode: "M4 4GL",
+    about_me:
+      "Aenean lectus. Pellentesque eget nunc. Donec quis orci eget orci vehicula condimentum.",
+  });
+
+  const [userPosition, setUserPosition] = useState({lat:null,lon:null});
   return (
-    <UserContext.Provider value={{ user, setUser }}>
+    <UserContext.Provider value={{ user, setUser, userPosition, setUserPosition }}>
       {children}
     </UserContext.Provider>
   );
