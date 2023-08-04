d2823bb21059ff5f8eb28bb93d1e307f75ed50a8
catherine-newman <3197941+catherine-newman@users.noreply.github.com>
2023-08-04 10:46:16 +0100

    add getSingleUser and getEvents

--- utils/api.js
+++ utils/api.js
@@ -4,12 +4,38 @@ const condivisoApi = axios.create({
     baseURL: "https://condiviso-be.onrender.com/api"
 });
 
-const getSingleEvent = (eventId) => {
+getSingleEvent = (eventId) => {
     const endpoint = `/events/${eventId}`;
     return condivisoApi.get(endpoint).then((res) => {
         return res.data.event;
     })
 }
 
+getSingleUser = (user_id) => {
+    const endpoint = `/users/${user_id}`;
+    return condivisoApi.get(endpoint).then((res) => {
+        return res.data;
+    })
+}
 
-module.exports = getSingleEvent;
+getEvents = async (user_id, from_date, to_date, lon, lat, dist, unit, spaces) => {
+    const params = {};
+    if (user_id) params.user_id = user_id;
+    if (from_date) params.from_date = from_date;
+    if (to_date) params.to_date = to_date;
+    if (lon) params.lon = lon;
+    if (lat) params.lat = lat;
+    if (dist) params.dist = dist;
+    if (unit) params.unit = unit;
+    if (spaces) params.spaces = spaces;
+    const res = await condivisoApi.get(`/events`, {
+        params: params
+    });
+    return res.data;
+}
+
+module.exports = {
+    getSingleEvent,
+    getSingleUser,
+    getEvents
+}
